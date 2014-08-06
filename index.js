ec551423ff8b606b4fa1a748d8a6c56e0b7ec361
heatroom <2257505815@qq.com>
2014-08-06 18:19:35 +0800

    add city json

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-var exec = require('child_process').exec;
-var request = require('superagent');
-var fs = require('fs');
 var Nightmare = require('nightmare');
-var cheerio = require('cheerio');
 var Speiyou = require('./lib/speiyou.js');
 
 //http://sbj.speiyou.com/search/index/grade:0/level:bx/subject:/gtype:time/service:/time:/term:/period:/o:da/bg:n/curpage:2
@@ -20,17 +16,6 @@ var Speiyou = require('./lib/speiyou.js');
  * @curpage "4"
  */
 
-console.log('start');
-
-var nightmare = new Nightmare();
-
-var parse = function(halfSum, nextSum) {
-  console.log("sum = " + (Number(halfSum) + Number(nextSum)));
-};
-
-nightmare
+new Nightmare
   .use(Speiyou.fetch())
-  .run(function(err, nightmare) {
-    if (err) return console.log();
-    console.log();
-  });
+  .run();
