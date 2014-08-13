bf6f69862b415def5b6c05b12c8ef752ad2af4f9
heatroom <2257505815@qq.com>
2014-08-13 21:18:27 +0800

    shift to mysql.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Nightmare = require('nightmare');
 var speiyou = require('./lib/speiyou');
+var mysql = require('mysql');
 
 //http://sbj.speiyou.com/search/index/grade:0/level:bx/subject:/gtype:time/service:/time:/term:/period:/o:da/bg:n/curpage:2
 /**
@@ -18,9 +19,23 @@ var speiyou = require('./lib/speiyou');
 
 var nightmare = new Nightmare();
 
+
+
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  port     : 3306,
+  database : 'blog',
+  user     : 'blog',
+  password : 'blog',
+  charset  : 'UTF8'
+});
+
+connection.connect();
+
 nightmare
-  .use(speiyou.fetch())
+  .use(speiyou.fetch(connection))
   .run(function(err, nightmare) {
   	if (err) return console.log(err);
   	console.log("over");
+    connection.end();
   });
